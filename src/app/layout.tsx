2b28a1bf63d726577caa902becdc8dcb2f7426f1
Kaptelinin <100-21@mail.ru>
2026-09-14 18:47:58 +0300

    Navbar created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Header/>
+      <Header />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-
-
 export default function Home() {
-  return (
-    <h1>HELLO FROM HOME</h1>
-  );
+  return <h1>HELLO FROM HOME</h1>;
 }
