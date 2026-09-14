302c8481192abe343b81363e90c15bf7ae1cdb6c
Kaptelinin <100-21@mail.ru>
2026-09-14 14:21:17 +0300

    Start of quiz constructor project

---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,13 @@
+// src/widgets/header/ui/Header.tsx
+'use client';
+
+import { FC } from 'react';
+import { Navbar } from '@/widgets/navbar';
+
+export const Header: FC = () => {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-default-200 bg-background/80 backdrop-blur-md">
+      <Navbar />
+    </header>
+  );
+};
---
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { FC } from 'react';
+
+export const Navbar: FC = () => {
+  return (
+    <nav className="sticky top-0 z-50 w-full border-b border-gray-200 bg-white/80 backdrop-blur-md">
+      <div className="container mx-auto flex h-16 max-w-7xl items-center justify-between px-6">
+        
+      </div>
+    </nav>
+  );
+};
